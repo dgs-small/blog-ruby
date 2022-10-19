a47c8da77e4d98dbd812a419cd94ad86b3b51868
dgs-small <lucas@pop-os.localdomain>
2022-10-18 21:07:05 -0300

    creating the new and create methods inside articles controller, and creating a form using form builder

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,28 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
+
+  #Shows a single article, which is found by its ID
+  def show 
+    @article = Article.find(params[:id])
+  end  
+
+  #Creates an Article instance to use in a form inside a view
+  def new
+    @article = Article.new
+  end
+
+  #method used tomake some validations and save the object on the database
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else  
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
 end
