b1003cff93659e6b25e73735ad9b0dd8a44b126d
dgs-small <lucas9667@hotmail.com>
2022-10-19 18:19:44 -0300

    Using partials templates and finishing the CRUD basics

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
 
   #method used tomake some validations and save the object on the database
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(article_params)
 
     if @article.save
       redirect_to @article
@@ -24,5 +24,31 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+
+  private 
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
   
 end
